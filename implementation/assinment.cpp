651c4129f08e80daeba26096661a9c656caf3d87
1305 <60752308+Oasis000@users.noreply.github.com>
2023-11-09 19:43:54 +0900

    프그 과제 해결하기
    해결 완.

--- implementation/assinment.cpp
+++ implementation/assinment.cpp
@@ -1,4 +1,5 @@
 //프그 lv2 과제 진행하기
+//복잡함 75 -> 100점 : 현재시간 갱신 안해준 부분 있었음.
 
 #include <iostream>
 #include <string>
@@ -23,9 +24,7 @@ vector<string> solution(vector<vector<string>> plans) {
         vec.push_back({ change(plans[i][1]),stoi(plans[i][2]), i });
     }
     sort(vec.begin(), vec.end());
-    for (int i = 0; i < plans.size(); i++) {
-        cout << vec[i][0] << " " << vec[i][1] <<" " << plans[vec[i][2]][0] << endl;
-    }
+
     int index = 0;
     while (index < plans.size()) {
         //마지막 과목인 경우
@@ -39,14 +38,11 @@ vector<string> solution(vector<vector<string>> plans) {
             int cur_end_time = vec[index][0] + vec[index][1];
             if (next_start_time < cur_end_time) {
                 //다음 과제를 해야하는 경우 스위치한다.
-                cout << "다음과제로 스위치 " << next_start_time << " " << cur_end_time << endl;
                 time = next_start_time;
                 //다 못한 과제를 스택에 넣어 줌.
-                s.push({ vec[index][1] - (vec[index + 1][0] - vec[index][0]), vec[index][2] });
-                cout << "s.top is " << s.top()[0] << " " << s.top()[1] << endl;
+                s.push({ (cur_end_time - next_start_time), vec[index][2] });
             }
             else {
-                cout << "과제 완료 " << cur_end_time << plans[vec[index][2]][0] << endl;
                 answer.push_back(plans[vec[index][2]][0]);
                 time = cur_end_time;
                 index++;
@@ -55,41 +51,26 @@ vector<string> solution(vector<vector<string>> plans) {
             index++;
         }
         //중간에 term이 생기는 경우 -> 기존에 하던 과제를 끝낸다.
-        //time 설정??
         while (!s.empty()) {
             int next_start_time = vec[index][0];
             int left_min = s.top()[0];
-            cout << "기존 과제 실행 " << endl;
-            cout << time + left_min << " " << next_start_time << endl;
             if (time + left_min > next_start_time) { //과제를 다 못함
                 s.top()[0] = (left_min - (next_start_time - time));
                 break;
             }
             else { //과제를 다 함.
-                cout << "과제 완 " << plans[s.top()[1]][0] << endl;
                 answer.push_back(plans[s.top()[1]][0]);
+                //여기부분 때문에 75점.
+                time = time + left_min;
                 s.pop();
             }
         }
-        
+
     }
     //남은 과제만 다 하면 되는 경우
     while (!s.empty()) {
         answer.push_back(plans[s.top()[1]][0]);
-        cout << "과제 완 " << plans[s.top()[1]][0] << endl;
         s.pop();
     }
     return answer;
-}
-
-int main() {
-    vector<vector<string>>plan = { {"korean", "11:40", "30"},{"english", "11:10", "20"},{"math", "11:20", "40"} };
-    //vector<vector<string>>plan = { {"science", "12:40", "50"},{"music", "12:20", "40"},{"history", "14:00", "30"},{"computer", "12:30", "100"} };
-    //vector<vector<string>>plan = { {"A", "11:40", "30"},{"B", "12:10", "20"},
-      //  {"C", "12:30", "40"} ,{"D", "12:50", "40"} ,{"E", "13:30", "40"} ,{"F", "11:30", "40"} };
-
-    vector<string>ans = solution(plan);
-    for (int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << endl;
-    }
 }
